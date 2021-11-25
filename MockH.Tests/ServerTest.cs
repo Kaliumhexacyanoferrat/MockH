dec67cf462d62a3d2aa763e2fb7dc096c9fc0807
Andreas Nägeli <andr.naegeli@gmail.com>
2021-11-26 00:38:18 +0100

    Add first set of HTTP methods

--- MockH.Tests/ServerTest.cs
+++ MockH.Tests/ServerTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using MockH.Environment;
 
 namespace MockH.Tests
--- MockH/On.cs
+++ MockH/On.cs
@@ -1,5 +1,6 @@
-﻿using GenHTTP.Api.Protocol;
-using MockH.Builder;
+﻿using MockH.Builder;
+
+using GenHTTP.Api.Protocol;
 
 namespace MockH
 {
@@ -7,7 +8,15 @@ namespace MockH
     public static class On
     {
 
+        public static IncompleteRule Head(string? path = null) => new(RequestMethod.HEAD, path);
+
         public static IncompleteRule Get(string? path = null) => new(RequestMethod.GET, path);
+
+        public static IncompleteRule Post(string? path = null) => new(RequestMethod.POST, path);
+
+        public static IncompleteRule Put(string? path = null) => new(RequestMethod.PUT, path);
+
+        public static IncompleteRule Delete(string? path = null) => new(RequestMethod.DELETE, path);
 
     }
 
