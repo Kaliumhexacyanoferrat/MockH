d849259125be0e1a4e404d3cbfe481c23fb1acd4
Andreas Nägeli <andr.naegeli@gmail.com>
2021-12-03 18:02:01 +0100

    Implement additional functionaly

---
+++ MockH.Tests/RedirectTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace MockH.Tests
+{
+
+    [TestClass]
+    public class RedirectTests : ServerTest
+    {
+
+        [TestMethod]
+        public async Task RulesCanRedirectTemporarily()
+        {
+            using var server = MockServer.Run
+            (
+                On.Get().Redirect("https://www.google.de")
+            );
+
+            using var response = await GetAsync(server);
+
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
+            Assert.AreEqual(new Uri("https://www.google.de"), response.Headers.Location);
+        }
+
+        [TestMethod]
+        public async Task RulesCanRedirectPermanently()
+        {
+            using var server = MockServer.Run
+            (
+                On.Get().Redirect("https://www.google.de", temporary: false)
+            );
+
+            using var response = await GetAsync(server);
+
+            Assert.AreEqual(HttpStatusCode.Moved, response.StatusCode);
+            Assert.AreEqual(new Uri("https://www.google.de"), response.Headers.Location);
+        }
+
+    }
+
+}
